0160fb34af1ed7450789cf74f797f43b6e9f8074
Thien Tran <fcduythien@gmail.com>
2017-04-03 23:11:13 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<img align="right" width="175px" src="http://i.imgur.com/mdZ8Ktf.png" />
+
+# Phanbook Docker
+
+[![Version 2.2.1](https://img.shields.io/badge/version-v2.2.1-green.svg)][:release:]
+[![Build Status](https://travis-ci.org/phalcon/phalcon-compose.svg?branch=master)][:status:]
+[![Phalcon v3.0.4](https://img.shields.io/badge/phalcon-3.0.4-blue.svg)][:phalcon:]
+[![Downloads](https://img.shields.io/packagist/dt/phalcon/compose.svg)][:downloads:]
+
+The official Phanbook Docker Edition
+
+This is an official, open-source and community-driven boilerplate for Phanbook projects that run on [Docker][:docker:].
+
+### Installing
+
+You can take docs at https://github.com/phanbook/docs/blob/master/docker.md
+
+Inspiring by https://github.com/phalcon/phalcon-compose
---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#
+# Executes setup.rb using the Ruby bundled with Vagrant.
+#
+
+if [ ! -d "phanbook" ]; then
+	git clone --depth=1 https://github.com/phanbook/phanbook.git
+fi
+
+
+if ! which docker > /dev/null; then
+	echo "Docker doesn't seem to be installed. Please download and install it"
+	echo "from https://docs.docker.com/engine/installation/ and re-run phanbook."
+	exit 3
+fi
+if [[ "destroy" == "$2" ]]; then
+	# Stop and remove any existing containers
+	docker rm -f $(docker ps -a -q)
+	# Delete all images
+	docker rmi -f $(docker images -q)
+fi
+if ! which docker-compose > /dev/null; then
+	curl -L https://github.com/docker/compose/releases/download/1.3.3/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+	chmod +x /usr/local/bin/docker-compose
+	sudo service docker start
+fi
+cp phanbook/{.env.example,.env}
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+	sed -i 's|DB_HOST=localhost|DB_HOST=mysql|g' phanbook/.env
+else
+
+	sed -i '' 's|DB_HOST=localhost|DB_HOST=mysql|g' phanbook/.env
+fi
+docker-compose up -d
+
+# Docker setup
+
+
+
+
+
